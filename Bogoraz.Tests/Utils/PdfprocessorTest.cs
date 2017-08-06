632ac793c6bf589e155c0bf9d95d210eaacb90dd
Kelman, Yevgeniy <Yevgeniy.Kelman@capitalone.com>
2017-08-06 18:50:28 -0500

    Added PDFProcessor

---
+++ Bogoraz.Tests/Utils/PdfprocessorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Bogoraz.Utils;
+using Bogoraz.Models;
+
+namespace Bogoraz.Tests.Utils
+{
+    [TestClass]
+    public class PdfprocessorTest
+    {
+        [TestMethod]
+        public void TestUpdatePDF()
+        {
+            Application application = new Application();
+            application.FirstName = "John";
+            application.LastName = "Johnson";
+
+            String inputPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\Docs\\BLGLegal Intake Form AcroForm.pdf";
+            String outputPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\Docs\\BLGLegal Intake Form_new.pdf";
+
+            PdfProcessor pdfproc = new PdfProcessor();
+            pdfproc.UpdatePDF(outputPath, inputPath, application);
+
+        }
+    }
+}
---
+++ Bogoraz/Utils/PdfProcessor.cs
@@ -0,0 +1,27 @@
+﻿using Bogoraz.Models;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Bogoraz.Utils
+{
+    public class PdfProcessor
+    {
+        public void UpdatePDF(string outputPath, string inputPath, Application application)
+        {
+            using (FileStream outFile = new FileStream(outputPath, FileMode.Create))
+            {
+                PdfReader pdfReader = new PdfReader(inputPath);
+                PdfStamper pdfStamper = new PdfStamper(pdfReader, outFile);
+                AcroFields fields = pdfStamper.AcroFields;
+                fields.SetField("Client Name", application.FirstName + " " + application.LastName);
+                pdfStamper.Close();
+                pdfReader.Close();
+            }
+        }
+        
+    }
+}
